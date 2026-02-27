9ba1812c45e3255f15fd052bd46515f7e3db9fc7
aamir gheewale <aamirgheewale142210@gmail.com>
2026-02-27 13:09:31 +0530

    chore: regenerate Convex API types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -12,6 +12,7 @@ import type * as auth from "../auth.js";
 import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
 import type * as messages from "../messages.js";
+import type * as reactions from "../reactions.js";
 import type * as typing from "../typing.js";
 import type * as unread from "../unread.js";
 import type * as users from "../users.js";
@@ -27,6 +28,7 @@ declare const fullApi: ApiFromModules<{
   conversations: typeof conversations;
   http: typeof http;
   messages: typeof messages;
+  reactions: typeof reactions;
   typing: typeof typing;
   unread: typeof unread;
   users: typeof users;
